de9bbeeed3c2e31ec42679dc648da23d993d20f9
Saloni Jangid <salonijangid04@gmail.com>
2024-04-09 13:13:55 +0530

    second

--- bitwise operators/setbits.java
+++ bitwise operators/setbits.java
@@ -2,6 +2,11 @@ public class setbits {
         public static void main(String[] args) {
         //find num of set bits i.e 1's
         int n=9;
+        /*
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toHexString(n));
+        System.out.println(Integer.toOctalString(n));
+        */
         int count=0;
         while(n>0){
             if((n & 1) == 1){
