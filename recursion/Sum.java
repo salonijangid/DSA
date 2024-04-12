e5d83cfc728155baffa703616a89e97c75b3ab7d
Saloni Jangid <salonijangid04@gmail.com>
2024-04-12 12:52:35 +0530

    first

---
+++ recursion/Sum.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Sum {
+    public static void main(String[] args) {
+        //sum of 1 to n
+        int ans=sum(5);
+        System.out.println(ans);
+    }
+    
+    static int sum(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+}
---
+++ recursion/numbers.java
@@ -0,0 +1,26 @@
+package recursion;
+
+public class numbers {
+    public static void main(String[] args) {
+        fun(5);
+        fun_rev(5);
+    }
+
+    //print from n to 1
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n);
+        fun(n-1);
+    }
+
+    //print from 1 to n
+    static void fun_rev(int n) {
+        if (n == 0) {
+            return;
+        }
+        fun_rev(n-1);
+        System.out.print(n);
+    }
+}
