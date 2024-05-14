88e5ca829c1334f619919687703e1263f26850f8
Saloni Jangid <salonijangid04@gmail.com>
2024-05-14 22:36:32 +0530

    string

---
+++ strings/pangram.java
@@ -0,0 +1,23 @@
+package strings;
+
+//pangram - all letters appear at least once
+
+public class pangram {
+    public static void main(String[] args) {
+        String sentence = "hello";
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    public static boolean checkIfPangram(String sentence) {
+        sentence = sentence.toLowerCase();
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        //convert the string to a char array and check
+        for(char ch : letters.toCharArray()){
+            //if it doesnt contain all the letters
+            if(!sentence.contains(String.valueOf(ch))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
