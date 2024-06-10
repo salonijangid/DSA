e40b6a1e89032a3a52a892dc12de9958b7cd2fe1
Saloni Jangid <salonijangid04@gmail.com>
2024-06-10 22:05:50 +0530

    arr-h

---
+++ arrays/arrays_hard/three_sum.java
@@ -0,0 +1,23 @@
+package arrays_hard;
+
+import java.util.Arrays;
+
+public class three_sum {
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,-2};
+        System.out.println(Arrays.toString(sum(arr)));
+    }
+
+    static int[] sum(int[] arr){
+        for(int i=0; i<arr.length;i++){
+            for(int j=i+1; j<arr.length; j++){
+                for(int k=j+1; k<arr.length; k++){
+                    if(i!=j && i!=k && j!=k && arr[i]+arr[j]+arr[k]==0){
+                        return new int[] {arr[i], arr[j], arr[k]};
+                    }
+                }
+            }
+        }
+        return new int[] {0,0,0};
+    }
+}
