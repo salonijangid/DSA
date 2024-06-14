4af3663d04b68d41325dfaeb1ad5f07bf172c5ea
Saloni Jangid <salonijangid04@gmail.com>
2024-06-14 21:00:19 +0530

    arr-easy

---
+++ arrays/arrays_easy/left_rotate.java
@@ -0,0 +1,20 @@
+package arrays_easy;
+
+import java.util.Arrays;
+
+public class left_rotate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        System.out.println(Arrays.toString(rotate(arr)));
+    }
+
+    static int[] rotate(int[] arr){
+        int n = arr.length;
+        int temp=arr[0];
+        for(int i=0; i<n-1; i++){
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=temp;
+        return arr;
+    }
+}
